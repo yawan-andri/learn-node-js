b261a8d6c29bc376fda6f7d431d48670839011fb
yawan <yawan.andrisetyawan@gmail.com>
2024-09-19 16:22:01 +0700

    Add files via upload

---
+++ events/index.js
@@ -0,0 +1,11 @@
+const { EventEmitter } = require('events');
+
+const birthdayEventListener = (name) => {
+    console.log(`Happy birthday ${name}!`);
+}
+
+const myEventEmitter = new EventEmitter();
+
+myEventEmitter.on('birthday', birthdayEventListener);
+
+myEventEmitter.emit('birthday', 'Babi');
